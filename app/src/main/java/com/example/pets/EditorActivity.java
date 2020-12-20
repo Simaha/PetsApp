a2a7edaab1930680f878cb99bbd386016de4675b
Simaha <simahaebrima@gmail.com>
2020-12-20 04:38:53 +0000

    add PetContract Class

--- app/src/main/java/com/example/pets/EditorActivity.java
+++ app/src/main/java/com/example/pets/EditorActivity.java
@@ -16,6 +16,9 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NavUtils;
 
+import com.example.pets.data.PetContract;
+import com.example.pets.data.PetContract.PetEntry;
+
 /**
  * Allows user to create a new pet or edit an existing one
  */
@@ -75,11 +78,11 @@ public class EditorActivity extends AppCompatActivity {
                 String selection = (String) parent.getItemAtPosition(position);
                 if (!TextUtils.isEmpty(selection)) {
                     if (selection.equals(getString(R.string.gender_male))) {
-                        mGender = 1; // Male
+                        mGender = PetEntry.GENDER_MALE; // Male
                     } else if (selection.equals(getString(R.string.gender_female))) {
-                        mGender = 2; // Female
+                        mGender = PetEntry.GENDER_FEMALE; // Female
                     } else {
-                        mGender = 0; // Unknown
+                        mGender = PetEntry.GENDER_UNKNOWN; // Unknown
                     }
                 }
             }
