1934af585879e0c47809ed0b5daaa30466902576
Simaha <simahaebrima@gmail.com>
2020-12-28 00:52:52 +0000

    updated CatalogActivity Menu Options

--- app/src/main/java/com/example/pets/CatalogActivity.java
+++ app/src/main/java/com/example/pets/CatalogActivity.java
@@ -1,11 +1,14 @@
 package com.example.pets;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -33,7 +36,9 @@ public class CatalogActivity extends AppCompatActivity {
             }
         });
 
-        displayDatabaseInfo();
+        PetDbHelper mDbHelper = new PetDbHelper(this);
+        //SQLiteDatabase db = mDbHelper.getReadableDatabase();
+        //displayDatabaseInfo();
     }
 
     /**
@@ -61,5 +66,26 @@ public class CatalogActivity extends AppCompatActivity {
             // resources and makes it invalid.
             cursor.close();
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_catalog, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //User clicked on a menu option in the app bar overflow menu
+        switch (item.getItemId()){
+            //Respond to a click on the "Insert dummy data" menu option
+            case R.id.action_insert_dummy_data:
+                return true;
+
+            case R.id.action_delete_all_entries:
+                //Don nothing for now
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
