cf3ef9c248f9cd3091c236b1cb153fda6c528b33
Simaha <simahaebrima@gmail.com>
2021-01-07 20:33:25 +0000

    added uri matcher code

--- app/src/main/java/com/example/pets/data/PetProvider.java
+++ app/src/main/java/com/example/pets/data/PetProvider.java
@@ -2,6 +2,7 @@ package com.example.pets.data;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.UriMatcher;
 import android.database.Cursor;
 import android.net.Uri;
 
@@ -15,6 +16,19 @@ public class PetProvider extends ContentProvider {
 
     /** Tag for the log messages */
     public static final String LOG_TAG = PetProvider.class.getSimpleName();
+
+    /** URI matcher code for the content URI for the pets table */
+   private static final int PETS = 100;
+
+    /** URI matcher code for the content URI for a single pet in the pets table */
+    private static final int PET_ID = 101;
+
+    /**
+     * UriMatcher object to match a content URI to a corresponding code.
+     * The input passed into the constructor represents the code to return for the root URI.
+     * It's common to use NO_MATCH as the input for this case.
+     */
+    private static final UriMatcher sUriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
 
     /**
      * Initialize the provider and the database helper object.
@@ -63,9 +77,5 @@ public class PetProvider extends ContentProvider {
     public String getType(Uri uri) {
         return null;
     }
-
-
-
-
 
 }
